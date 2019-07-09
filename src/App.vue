ea066e2aac426b1b6cea8c084cf62cadc46b6864
Tony <1332988826@qq.com>
2019-07-09 08:20:45 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <v-app>
+      <img alt="Vue logo" src="./assets/logo.png">
+      <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </v-app>
   </div>
 </template>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+import zhHans from 'vuetify/es5/locale/zh-Hans'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#9C27B0'
+  },
+  lang: {
+    locales: { zhHans },
+    current: 'zhHans'
+  }
+})
 
 Vue.config.productionTip = false
 
